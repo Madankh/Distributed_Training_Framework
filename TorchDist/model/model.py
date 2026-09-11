dd88390035f0697c0e7d71de0114af43736653b9
Madankh <khadkasuman0000@gmail.com>
2026-09-11 13:22:14 +0545

    init_weights incomplete

--- TorchDist/model/model.py
+++ TorchDist/model/model.py
@@ -12,3 +12,24 @@ class DeepSeekV3model(nn.Module):
         super().__init__()
         self.model_args = args
         self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
+        self.register_buffer("freqs_cis", precompute_freqs_cis(args), persistent=False)
+        self.layers = torch.nn.ModuleDict()
+        for layer_idx in range(args.layers):
+            self.layers[str(layer_idx)] = TransformerBlock(layer_idx, args)
+
+        self.norm = nn.RMSNorm(args.dim)
+        self.output = nn.Linear(
+            args.dim, 
+            args.vocab_size, 
+            dtype=torch.get_default_dtype(),
+            bias=False,
+        )
+
+
+    def init_weights(
+        self,
+        init_std:float | None = None,
+        buffer_device:torch.device | None = None
+    ):
+        buffer_device = buffer_device or  self.freqs_cis.device
+
