32a38de36f9bd40202c0ddf2a1d72d5bbeadf60d
Madankh <khadkasuman0000@gmail.com>
2026-09-14 10:55:20 +0545

    transformer Block code

--- TorchDist/model/model.py
+++ TorchDist/model/model.py
@@ -7,6 +7,51 @@ from TorchDist.model.model_args import DeepSeekV3Args
 from TorchDist.model.moe import FeedForward, MoE
 from TorchDist.model.rope import apply_rotary_emb, precompute_freqs_cis
 
+class TransformerBlock(nn.Module):
+    def __init__(self, layer_id:int, args:DeepSeekV3Args):
+        super().__init__()
+        self.attention = Attention(args)
+        self.attention_norm = nn.RMSNorm(args.dim, eps=args.norm_eps)
+        self.ffn_norm = nn.RMSNorm(args.dim,eps=args.norm_eps)
+
+        self.moe_enabled = layer_id >= args.n_dense_layers
+        if self.moe_enabled:
+            self.moe = MoE(
+                args.moe.args,
+                dim=args.dim,
+                hidden_dim=args.moe_inter_dim
+            )
+        else:
+            self.feed_forward = FeedForward(args.dim, args.inter_dim)
+        self.weight_init_std = 0.02 / (2 * (layer_id + 1)) ** 0.5
+        self.layer_id = layer_id
+
+    def forward(self, x:torch.Tensor, freqs_cis:torch.Tensor):
+        x = x + self.attention(self.attention_norm(x),freqs_cis)
+        if self.moe_enabled:
+            x = x + self.moe(self.ffn_norm(x))
+        else:
+            x = x + self.feed_forward(self.ffn_norm(x))
+        return x
+
+    def init_weights(
+        self,
+        init_std:float | None = None,
+        buffer_device:torch.device | None = None
+    ):
+     if buffer_device is None:
+        raise ValueError("buffer_device must be provided for TransformerBlock weight init")
+     for norm in (self.attention_norm, self.ffn_norm):
+        norm.reset_parameters()
+     self.attention.init_weights(self.weight_init_std)
+     if self.moe_enabled:
+        self.moe.init_weights(
+            init_std=self.weight_init_std,buffer_device=buffer_device
+        )
+     else:
+        self.feed_forward.init_weights(self.weight_init_std)
+
+
 class DeepSeekV3model(nn.Module):
     def __init__(self, args:DeepSeekV3Args):
         super().__init__()
